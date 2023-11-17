4dcf6ff56b656d0e595005510c602e3a276b0956
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2023-11-17 14:47:44 +0100

    Hardcode quote

--- src/Inspire.php
+++ src/Inspire.php
@@ -2,14 +2,10 @@
 
 namespace Sakac\Inspire;
 
-use Illuminate\Support\Facades\Http;
-
 class Inspire
 {
     public function justDoIt(): string
     {
-        $response = Http::get('https://inspiration.goprogram.ai/');
-
-        return $response['quote'] . ' -' . $response['author'];
+        return 'Lorem ipsum';
     }
 }
