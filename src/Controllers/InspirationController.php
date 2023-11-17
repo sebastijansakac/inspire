9e29bad62bb6abbf934f2bfdd6d39eec89c82757
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2023-11-17 13:50:23 +0100

    Add swagger docs

--- src/Controllers/InspirationController.php
+++ src/Controllers/InspirationController.php
@@ -2,10 +2,37 @@
 
 namespace Sakac\Inspire\Controllers;
 
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\Response;
 use Sakac\Inspire\Inspire;
 
 class InspirationController
 {
+    #[Get(
+        path: '/api/v1/inspire',
+        summary: 'Get user data',
+        tags: [
+            'Inspire',
+        ],
+        responses: [
+            new Response(
+                response: 200,
+                description: 'Success page',
+            ),
+            new Response(
+                response: 400,
+                description: 'Bad Request',
+            ),
+            new Response(
+                response: 401,
+                description: 'Unauthorized',
+            ),
+            new Response(
+                response: 404,
+                description: 'Not found',
+            ),
+        ],
+    )]
     public function __invoke(Inspire $inspire): string
     {
         $quote = $inspire->justDoIt();
