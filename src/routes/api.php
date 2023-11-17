9876a3c587e35487b3528422e55468fa1be57bc8
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2023-11-17 14:41:48 +0100

    Update route prefix

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,9 +4,9 @@ use Sakac\Inspire\Controllers\InspirationController;
 
 Route::group(['middleware' => ['auth:sanctum']], static function () {
     # USER ROUTES
-    Route::prefix('inspire')->name('inpire.')->group(static function () {
+    Route::prefix('inspire')->name('inspire.')->group(static function () {
         Route::prefix('api/v1')
             ->name('api.v1.')
-            ->get('inspire', InspirationController::class)->name('single');
+            ->get('', InspirationController::class)->name('single');
     });
 });
